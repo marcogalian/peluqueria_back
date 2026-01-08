fe855587d7f7b7a0c5a9cf943fb8e70dbc89a1d6
marco <marcogalian@hotmail.com>
2026-01-08 12:36:44 +0100

    Creación estructura servicios

---
+++ src/main/java/com/marcog/peluqueria/servicios/domain/model/Servicio.java
@@ -0,0 +1,21 @@
+package com.marcog.peluqueria.servicios.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Servicio {
+    private UUID id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private Integer duracionMinutos; // Asi se puede saber cuanto tiempo bloquea la agenda a cada peluquer@.
+}
